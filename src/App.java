67c7bd313378113abbe453f0be3f7d131ab23cfc
Vinicius Ferreira <raymundo.vinicius@zup.com.br>
2024-07-22 12:02:07 -0300

    Create task

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) {
+        // Faça um vetor de duas dimensões com 3 posições relacionando o faturamento de uma empresa.
+        // o ultimo index de cada posição corresponde ao faturamento de cada trimestre
+        // 1) faça um programa que calcule o total do faturamento do ano todo.
+        // 2) faça um programa que calcule o total do faturamento do meio de cada trimestre
+        // 3) faça um programa que calcule o total do faturamento do inicio do ultimo trimestre, medio do segundo e final do primeiro.
 
+        // exemplo da matriz
+        //    |0      |1      |2     |
+        // |0| 100,00 |237,84 |555,23|
+        // |1| 2000,00|4558,72|6534,2|
+        // |2| 300,00 |374,23 |765,23|
     }
 }
