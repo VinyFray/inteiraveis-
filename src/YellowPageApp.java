b9d6c0f8db6da2fb3fc33f8f53dbaa666cec3468
Vinicius Ferreira <raymundo.vinicius@zup.com.br>
2024-07-24 11:35:34 -0300

    Create task to yellow page app

---
+++ src/YellowPageApp.java
@@ -0,0 +1,10 @@
+public class YellowPageApp {
+    public static void main(String[] args) {
+//        Faça um programa que permita cadastrar nomes e sobrenome e números de telefones
+//        - O programa não deve armazenar nomes repetidos e nem numeros repetidos
+//        - o programa não pode aceitar numeros de telefone sem o dd
+//
+//        Melhoria do programa, faça com que seja possivel buscar o numero de telefone a partir do nome completo da
+//        pessoa, desconsiderando as letras maiúsculas
+    }
+}
