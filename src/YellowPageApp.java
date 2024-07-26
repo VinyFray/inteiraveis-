50107e78d148177e2df75e61fbab48fc3648a0bf
Vinicius Ferreira <raymundo.vinicius@zup.com.br>
2024-07-26 14:35:03 -0300

    Add bug list

--- src/YellowPageApp.java
+++ src/YellowPageApp.java
@@ -9,12 +9,21 @@ public class YellowPageApp {
 //        Melhoria do programa, faça com que seja possivel buscar o numero de telefone a partir do nome completo da
 //        pessoa, desconsiderando as letras maiúsculas
 //
+        // BUG 1 - Quando um numero de telefone é digitado errado o Nome é cadastrado e o numero não. Causando problemas
+        // de busca pelo indice
+        // exemplo: [Vinicius, Marco, Maria] [123, 456] o numero 456 na verdade é da Maria, porém ficou com indice do
+        // Marco, pois Marco digitou numero invalido
+        // BUG 2 - Nomes cadastrados com espaço no final não são encontrados
+        // BUG 3 - Nomes não devem existir sem numero de telefone.
+        // BUG 4 - Lista completa da errro de index não encontrado
+
         List<String> names = new ArrayList<>();
         List<String> phonesNumbers = new ArrayList<>();
 
         boolean control = true;
         while (control){
-            int menu = scanner("Digite: \n 1 para adicionar nome e telefone \n 2 para buscar por nome \n 0 para sair").nextInt();
+            int menu = scanner("Digite: \n 1 para adicionar nome e telefone \n 2 para buscar por nome \n 0 para sair " +
+                    "\n 3 para ver lista completa").nextInt();
             if(menu == 1) {
                 String nome = scanner("Digite o nome e sobrenome").nextLine();
                 if (!names.contains(nome)) names.add(nome);
@@ -41,6 +50,10 @@ public class YellowPageApp {
                 }
             }else if (menu == 0){
                 control = false;
+            } else if (menu == 3) {
+                for (String nameList : names){
+                    System.out.println(nameList+": "+phonesNumbers.get(nameList.indexOf(nameList))+"\n -------------");
+                }
             }
         }
     }
